941b5ccbf261eac20afcc093642110269b296cba
josimarsousa <josimarjose@hotmail.com>
2021-12-06 12:24:07 -0300

    react-refresh ok

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,11 @@
 //quando se usa state(estado) chama-se hook(gancho)
 import { useState } from "react";
 
+//usuarios -= ['diego3g', 'diegosf', 'danileao']
+//usarios.push('rafacamarda')
+//imutabilidade
+//-->novoUsuarios = [...usuarios, 'rafacamarda']
+
 export function Counter(){
   //desestruturacao
   const [counter, setCounter] = useState(0);
